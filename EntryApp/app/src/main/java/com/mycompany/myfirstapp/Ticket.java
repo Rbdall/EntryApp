44837cffdf69026152becdf1a2657d5ddf5755cb
Ryan Dall <rbdall@ucla.edu>
2015-05-19 15:05:43 -0700

    Ticket class and TicketList activity

---
+++ EntryApp/app/src/main/java/com/mycompany/myfirstapp/Ticket.java
@@ -0,0 +1,63 @@
+package com.mycompany.myfirstapp;
+
+/**
+ * Created by ryan on 5/19/15.
+ */
+public class Ticket {
+    private String ticketName;
+    private String venueName;
+    private String eventDate;
+    private String seatLocation;
+    private String ticketID;
+    private String ticketHolder;
+    private boolean validTicket;
+
+
+    public Ticket(String ticketName, String venueName, String eventDate, String seatLocation,
+                  String ticketID, String ticketHolder, boolean validTicket){
+        this.ticketName = ticketName;
+        this.venueName = venueName;
+        this.eventDate = eventDate;
+        this.seatLocation = seatLocation;
+        this.ticketID = ticketID;
+        this.ticketHolder=ticketHolder;
+        this.validTicket = validTicket;
+    }
+
+    public String getTicketName(){
+        return ticketName;
+    }
+
+    public String getVenueName(){
+        return venueName;
+    }
+
+    public String getEventDate(){
+        return eventDate;
+    }
+
+    public String getSeatLocation(){
+        return seatLocation;
+    }
+
+    public String getTicketID(){
+        return ticketID;
+    }
+
+    public String getTicketHolder(){
+        return ticketHolder;
+    }
+
+    public boolean isValidTicket(){
+        return validTicket;
+    }
+
+    public String toString(){
+        return ticketName + '\n' +
+                '\t' + "Venue: " + venueName + '\n' +
+                '\t' + "Date: " + eventDate + '\n' +
+                '\t' + seatLocation + '\n' +
+                '\t' + "TicketID: " + ticketID + '\n';
+    }
+
+}
