818d27e5d62a919c1dd181bec9f8453407f2a1bf
Ryan Dall <rbdall@ucla.edu>
2015-05-26 19:48:00 -0700

    Majority of the fan bluetooth code

--- EntryApp/app/src/main/java/com/mycompany/myfirstapp/FanSelectedTicket.java
+++ EntryApp/app/src/main/java/com/mycompany/myfirstapp/FanSelectedTicket.java
@@ -15,19 +15,160 @@ import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 
 public class FanSelectedTicket extends ActionBarActivity {
     private Ticket selectedTicket;
+
+    private BluetoothSocket mConnection = null;
+    private BluetoothAdapter mBluetoothAdapter;
 
     private final android.content.BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra("EXTRA_DEVICE");
-                System.out.print(intent.getStringExtra("EXTRA_DEVICE"));
+                if(mConnection == null) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    TextView ticketInformation = (TextView) findViewById(R.id.pairedText);
+                    ticketInformation.setText(intent.getStringExtra(BluetoothDevice.EXTRA_NAME));
+                    ConnectThread connection = new ConnectThread(device);
+                    connection.run();
+                    if(connection.getSocket() != null){
+                        mConnection = connection.getSocket();
+                        manageConnection();
+                    }
+
+                }
             }
         }
     };
+
+    public void manageConnection(){
+        rwThread thread = new rwThread(mConnection);
+        thread.write(selectedTicket.getTicketID().getBytes());
+        byte[] color = thread.read();
+        //show thing based on color
+        thread.write("ACK".getBytes());
+        byte[] validated = thread.read();
+        thread.write("ACK".getBytes());
+        thread.cancel();
+        //transition when the ticket is validated
+
+    }
+
+    private class rwThread extends Thread{
+        private BluetoothSocket mSocket;
+        private InputStream inStream;
+        private OutputStream outStream;
+
+        public byte lastRead;
+
+        public rwThread(BluetoothSocket socket){
+            mSocket = socket;
+
+            try{
+                inStream = mSocket.getInputStream();
+                outStream = mSocket.getOutputStream();
+            }
+            catch(IOException e){
+                inStream = null;
+                outStream = null;
+            }
+
+
+        }
+
+        public void run(){
+            byte[] buffer = new byte[1024];
+            while(true) {
+                try {
+                    lastRead = (byte) inStream.read(buffer);
+
+                } catch (IOException e) {
+
+                }
+            }
+        }
+
+        public byte[] read(){
+            byte[] buffer = new byte[1024];
+            try {
+                inStream.read(buffer);
+                return buffer;
+
+            } catch (IOException e) {
+
+            }
+            return null;
+        }
+        public void write(byte[] buffer) {
+            try {
+                outStream.write(buffer);
+            }
+            catch(IOException e) {
+
+            }
+        }
+
+        public void cancel(){
+            try{
+                mSocket.close();
+            }
+            catch(IOException cantClose){
+
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private BluetoothSocket mSocket;
+        private BluetoothDevice mDevice;
+
+        public ConnectThread(BluetoothDevice device){
+            try {
+                mDevice = device;
+                mSocket = mDevice.createRfcommSocketToServiceRecord(new UUID(0, 0));
+            }
+            catch (IOException e){
+
+            }
+        }
+
+        public BluetoothSocket getSocket(){
+            return mSocket;
+        }
+        public void run(){
+            mBluetoothAdapter.cancelDiscovery();
+            try{
+                mSocket.connect();
+            }
+            catch(IOException cantConnect){
+                try{
+                    mSocket.close();
+                    mSocket = null;
+                    mBluetoothAdapter.startDiscovery();
+                }
+                catch(IOException cantClose){
+
+                }
+                return;
+            }
+        }
+
+        public void cancel(){
+            try{
+                mSocket.close();
+                mBluetoothAdapter.startDiscovery();
+            }
+            catch(IOException cantClose){
+
+            }
+        }
+    }
 
 
     @Override
@@ -49,7 +190,7 @@ public class FanSelectedTicket extends ActionBarActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 showProgressBar(isChecked);
                 if(isChecked) {
-                    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
                     if (mBluetoothAdapter == null) {
                         //display bad things
                         System.out.println("Device has no bluetooth functionality");
@@ -74,7 +215,8 @@ public class FanSelectedTicket extends ActionBarActivity {
                     }
                 }
                 else{
-                    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    mConnection = null;
                     if (mBluetoothAdapter == null) {
                        //Nothing to turn off
                         return;
